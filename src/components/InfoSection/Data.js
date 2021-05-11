45f2e668ea0be2cd17e25789ec4fd6b3b51e819a
Dinesh Shrestha <dinu.shrestha777@gmail.com>
2021-05-11 15:51:04 +0200

    Simple react page

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,52 @@
+export const homeObjOne = {
+  id: 'about',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Premium Bank',
+  headline: 'Unlimited Transaction with zero fees',
+  description:
+    'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees',
+  buttonLabel: 'Get Started',
+  imgStart: false,
+  img: require('../../images/svg-1.svg').default,
+  alt: 'Car',
+  dark: true,
+  primary: true,
+  darkText: false,
+}
+
+export const homeObjTwo = {
+  id: 'discover',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Unlimited access',
+  headline: 'Login to your account at any time',
+  description: 'All you need is a phone and a computer to get connected',
+  buttonLabel: 'Learn More',
+  imgStart: true,
+  img: require('../../images/svg-2.svg').default,
+  alt: 'PiggyBank',
+  dark: false,
+  primary: false,
+  darkText: true,
+}
+
+export const homeObjThree = {
+  id: 'signup',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Join our Team',
+  headline: 'Creating an account is extremely easy',
+  description:
+    'Get everything setup and ready in under 10 minutes. All you need is add your information and you are ready to go.',
+  buttonLabel: 'Start Now',
+  imgStart: false,
+  img: require('../../images/svg-3.svg').default,
+  alt: 'Paper',
+  dark: false,
+  primary: false,
+  darkText: true,
+}
